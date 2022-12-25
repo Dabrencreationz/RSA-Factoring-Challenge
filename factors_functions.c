a9171ef4c29ea76e28671f5c89fd098c69672b0e
Brenda <brenvaltessy@yahoo.com>
2022-12-25 11:02:33 -0800

    RSA factoring challenge

--- factors_functions.c
+++ factors_functions.c
@@ -1,35 +1,29 @@
 #include <stdio.h>
+/*
+    Finds the smallest divisor, if any, of a given number `n`
+    Returns:
+        smallest divisor if found
+        0 if n is prime
+*/
+int trial_division(long int n){
+long int f;
 
-/**
- * Description: given factor of n
- * Finds the smallest divisor, if any, of a given number n.
- * Returns: smallest divis if found
- * 0 if n is prime
- */
+if (n%2 == 0){
+	printf("%lu=%lu*%i\n", n, n/2, 2);
+	return 0;
+}
 
-int trial_division(long int n)
+f = 3;
+while (f*f <= n)
 {
-	long int f;
-
-	(if (n % 2 == 0)
-	{
-		printf("%lu=%lu *%i\n", n, n / 2, 2);
-		return (0);
+	if (n%f == 0){
+		printf("%lu=%lu*%lu\n", n, n/f, f);
+		return 0;
 	}
-
-	f = 3;
-	while (f * f <= n)
-	{
-		if (n % f == 0)
-		{
-			printf("%lu=%lu*lu*\n", n, n / f, f);
-			return (0);
-		}
-		else
-		{
-			f += 2;
-		}
+	else{
+		f += 2;
 	}
-	printf("%lu=%lu*%i\n", n, n, 1);
-	return (0);
+}
+printf("%lu=%lu*%i\n", n, n, 1);
+return 0;
 }
